51fcdefbea58f6655e30246a3a7d95ff8247e7f7
H4ltor <timothee.cognard@ynov.com>
2021-04-09 15:56:35 +0200

    [Add] logout event

--- src/app/home/header-home/header-home.component.ts
+++ src/app/home/header-home/header-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/users/auth.service';
 
 @Component({
   selector: 'app-header-home',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderHomeComponent implements OnInit {
 
   
-  constructor() { }
+  constructor(public _authService: AuthService) { }
 
   ngOnInit(): void {
   }
--- src/app/users/auth.service.ts
+++ src/app/users/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable} from '@angular/core';
 import { HttpClient} from '@angular/common/http';
-
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
@@ -9,7 +9,8 @@ export class AuthService {
   private _registerUrl = "http://localhost:3000/api/register";
   private _loginUrl = "http://localhost:3000/api/login";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private _router: Router) { }
 
   registerUser(user: any) {
     return this.http.post<any>(this._registerUrl, user)
@@ -21,6 +22,11 @@ export class AuthService {
 
   loggedIn() {
     return !!localStorage.getItem('token')
+  }
+
+  logoutUser() {
+    localStorage.removeItem('token')
+    this._router.navigate(['/login'])
   }
 
   getToken() {
